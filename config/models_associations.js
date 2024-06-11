d4d6b7ae4214247f9f7555c4a703cf361713bc23
mohamedElnagar97 <mohamedelnaggar002@gmail.com>
2024-06-11 11:44:34 +0300

    add association between product and sales models

--- config/models_associations.js
+++ config/models_associations.js
@@ -1,1 +1,6 @@
-const User = require("../models/user");
+const User = require("../models/user.model");
+const Sales = require("../models/sales.model");
+const Product = require("../models/product.model");
+
+Product.hasMany(Sales);
+Sales.belongsTo(Product);
