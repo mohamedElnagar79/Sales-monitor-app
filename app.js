3f109a490f88b9bb4a765695a11e9b0891bb0bf8
mohamedElnagar97 <mohamedelnaggar002@gmail.com>
2024-06-11 14:13:28 +0300

    ad login route

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use([
   express.json({ limit: "50mb" }),
   express.urlencoded({ extended: false, limit: "50mb" }),
 ]);
-app.use([routes.userRoute, routes.productRoute]);
+app.use([routes.userRoute, routes.productRoute, routes.loginRoute]);
 
 app.use("/public", express.static(path.join(__dirname, "public")));
 // not found middleWare
--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,9 @@
 const userRoute = require("../routes/user.route");
 const productRoute = require("../routes/product.route");
+const loginRoute = require("../routes/login.route");
+
 module.exports = {
   userRoute,
   productRoute,
+  loginRoute,
 };
