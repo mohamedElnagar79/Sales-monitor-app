7e411520501dbfd36e4b77b9ff2aa914daa925b2
mohamedElnagaraffsquare <mohamedsaid@affsquare.com>
2024-06-30 20:15:17 +0300

    add returns model

---
+++ models/returns.model.js
@@ -0,0 +1,38 @@
+const Sequelize = require("sequelize");
+const sequelize = require("../config/database");
+
+const Returns = sequelize.define(
+  "returns",
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    quantity: {
+      type: Sequelize.INTEGER,
+      allowNull: true,
+    },
+    description: {
+      type: Sequelize.STRING(191),
+      required: false,
+      default: "",
+    },
+    reasone: {
+      type: Sequelize.STRING(191),
+      required: false,
+      default: "",
+    },
+    deletedAt: {
+      type: Sequelize.DATE,
+      allowNull: true,
+    },
+  },
+  {
+    paranoid: true,
+    deletedAt: "deletedAt",
+  }
+);
+
+module.exports = Returns;
