339ab78159aea4d0d3aa56d0fd96d7ec4466f016
mohamedElnagaraffsquare <mohamedsaid@affsquare.com>
2024-06-30 20:16:25 +0300

    add daily expense model

---
+++ models/Daily_expense.model.js
@@ -0,0 +1,44 @@
+const Sequelize = require("sequelize");
+const sequelize = require("../config/database");
+
+const DailyExpense = sequelize.define(
+  "daily_expenses",
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    amount: {
+      type: Sequelize.INTEGER,
+      allowNull: true,
+    },
+    quantity: {
+      type: Sequelize.INTEGER,
+      allowNull: true,
+    },
+    expenseName: {
+      type: Sequelize.STRING(191),
+      required: true,
+    },
+    description: {
+      type: Sequelize.STRING(191),
+      required: true,
+    },
+    reasone: {
+      type: Sequelize.STRING(191),
+      required: true,
+    },
+    deletedAt: {
+      type: Sequelize.DATE,
+      allowNull: true,
+    },
+  },
+  {
+    paranoid: true,
+    deletedAt: "deletedAt",
+  }
+);
+
+module.exports = DailyExpense;
