547f94717438c735425f3a7ac2d500df87b853d7
mohamedElnagaraffsquare <mohamedsaid@affsquare.com>
2024-06-30 20:15:42 +0300

    add association between returns and sales

--- config/models_associations.js
+++ config/models_associations.js
@@ -1,6 +1,10 @@
 const User = require("../models/user.model");
 const Sales = require("../models/sales.model");
 const Product = require("../models/product.model");
+const Returns = require("../models/returns.model");
 
 Product.hasMany(Sales);
 Sales.belongsTo(Product);
+
+Sales.hasMany(Returns);
+Returns.belongsTo(Sales);
