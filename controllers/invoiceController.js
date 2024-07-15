5fdd15f361dd024787eaf4ca2c663b3bd6871a44
mohamedElnagar97 <mohamedelnaggar002@gmail.com>
2024-07-15 09:44:34 +0300

    update get invoices function

--- controllers/invoiceController.js
+++ controllers/invoiceController.js
@@ -3,34 +3,55 @@ const moment = require("moment");
 const config = require("../config/middlewares");
 const Invoices = require("../models/invoice.model");
 const Clients = require("../models/clients.model");
+const InvoiceItems = require("../models/invoice_items.model");
 
 exports.getInvoices = async (req, res) => {
   try {
-    const search = req.query.search;
+    const searchDate = req.query.date ? moment(req.query.date) : moment();
 
-    const startOfYesterday = moment()
-      .subtract(1, "days")
-      .startOf("day")
-      .toDate();
-    const now = moment().toDate();
+    const startOfDay = searchDate.startOf("day").toDate();
+    const endOfDay = searchDate.endOf("day").toDate();
+
+    console.log("startOfDay: ", startOfDay);
+    console.log("endOfDay: ", endOfDay);
+
     const invoices = await Invoices.findAll({
       attributes: [
         "id",
-        [Sequelize.col("client.id"), "clientId"],
         [Sequelize.col("client.name"), "clientName"],
         "total",
         "amountPaid",
         "remainingBalance",
-        "comments",
         "createdAt",
+        [
+          Sequelize.literal(
+            "(SELECT COUNT(*) FROM invoice_items WHERE invoice_items.invoiceId = Invoices.id)"
+          ),
+          "itemCount",
+        ],
       ],
-      include: {
-        model: Clients,
-        required: false,
-        attributes: [],
+      include: [
+        {
+          model: Clients,
+          required: false,
+          attributes: [],
+        },
+        {
+          model: InvoiceItems,
+          required: false,
+          attributes: [],
+        },
+      ],
+
+      where: {
+        createdAt: {
+          [Op.gte]: startOfDay,
+          [Op.lt]: endOfDay,
+        },
       },
       order: [["createdAt", "DESC"]],
     });
+
     invoices.map((item) => {
       item.dataValues.createdAt = config.formatDate(item.dataValues.createdAt);
     });
